47a478a9562605d2095ac41c9f8c9d276dde076c
Matteo Peroni <matteoperoni.work@gmail.com>
2025-11-17 20:03:48 +0100

    Delete src/models/_init_models.py

--- src/models/_init_models.py
+++
@@ -1,9 +0,0 @@
-"""
-Models package for the BioMarket system.
-"""
-
-from src.models.product import Product
-from src.models.warehouse import Warehouse
-from src.models.sales import SalesManager
-
-__all__ = ['Product', 'Warehouse', 'SalesManager']
