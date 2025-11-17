9ef4566fd2508df54b98d54ac53adb38d8a0b414
Matteo Peroni <matteoperoni.work@gmail.com>
2025-11-17 20:04:42 +0100

    Update __init__.py

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,1 +1,8 @@
+"""
+Utilities package for the BioMarket system.
+"""
 
+from src.utils.input_validator import InputValidator
+from src.utils.logger import setup_logger
+
+__all__ = ['InputValidator', 'setup_logger']
